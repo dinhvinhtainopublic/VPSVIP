9e9e87743de1b2c1d9a0315369ac1856ceb09910
dinhvinhtainopublic <dinhvinhtai.2k9@gmail.com>
2025-12-27 10:27:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Start DBus
+# Khởi động DBus (cần cho LXDE chạy ổn)
 mkdir -p /var/run/dbus
 dbus-daemon --system --fork
 
-# Start SSH
-service ssh start
-
-# Prepare X11 directory
+# Chuẩn bị thư mục X11
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
-# Start XRDP
-/usr/sbin/xrdp --nodaemon
+# Start VNC server (màn hình desktop)
+vncserver :1 -geometry 1280x720 -depth 16
+
+# Start noVNC cho truy cập qua trình duyệt
+cd /opt/novnc
+./utils/launch.sh --vnc localhost:5901 --listen 8900
