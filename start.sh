b49ff89f8463a3adbb1fbaab4ba4a890fdb9164e
dinhvinhtainopublic <dinhvinhtai.2k9@gmail.com>
2025-12-27 16:18:47 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Khởi tạo thư mục SSH runtime
+# Tạo thư mục runtime giống bản gốc tạo folder trước khi chạy lệnh
 mkdir -p /var/run/sshd
 
-# Chạy SSH trên port 10000 (đã set trong Dockerfile)
+# Chạy SSH đúng port, không có apt, không cài gì thêm
 exec /usr/sbin/sshd -D -p 10000
